91f6d667e59a15d365247ee1d74262fb136daf51
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-07-09 09:32:34 +0200

    Revert "feature: add ae stylization"
    This reverts commit e753e1dfecc78f036ab84f5769e851f01a7f5daf.
    
    break: turn native to web application

--- 2kod/Views/MainWindow.axaml.cs
+++ 2kod/Views/MainWindow.axaml.cs
@@ -1,23 +1,26 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
 namespace _2kod.Views
 {
-    using Avalonia;
-    using Avalonia.Controls;
-    using Avalonia.Markup.Xaml;
+    using System;
 
-    /// <summary>
-    /// The main window.
-    /// </summary>
+    using Avalonia.Threading;
+
+    using ReactiveUI;
+
     public partial class MainWindow : Window
     {
 
         public MainWindow()
         {
             InitializeComponent();
+#if DEBUG
             this.AttachDevTools();
-
-            /*TextBox leftMwc = this.Get<TextEditor>("LeftMainWritableContent");
-            DispatcherTimer.RunOnce(() => leftMwc.Focus(), TimeSpan.FromMilliseconds(1));*/
-
+#endif
+            TextBox leftMwc = this.Get<TextBox>("LeftMainWritableContent");
+            DispatcherTimer.RunOnce(() => leftMwc.Focus(), TimeSpan.FromMilliseconds(1));
         }
 
         private void InitializeComponent()
